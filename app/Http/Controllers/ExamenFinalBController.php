199cf55a4535169af093d093790465c035a99850
Brandon <bampereza@miumg.edu.gt>
2023-06-10 07:49:16 -0600

    se agrego el controlador

---
+++ app/Http/Controllers/ExamenFinalBController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ExamenFinalB;
+use Illuminate\Http\Request;
+
+class ExamenFinalBController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ExamenFinalB  $examenFinalB
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ExamenFinalB $examenFinalB)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ExamenFinalB  $examenFinalB
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ExamenFinalB $examenFinalB)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ExamenFinalB  $examenFinalB
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ExamenFinalB $examenFinalB)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ExamenFinalB  $examenFinalB
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ExamenFinalB $examenFinalB)
+    {
+        //
+    }
+}
