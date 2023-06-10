48152dccae730cdac6b693256e54a6df8dc8ae00
mgeoher <mhernandezm85@miumg.edu.gt>
2023-06-10 08:29:36 -0600

    se crea una vista para la visualizacion del estudiante

--- resources/views/MelvinHernandez.blade.php
+++ resources/views/MelvinHernandez.blade.php
@@ -1,32 +1,35 @@
 @extends('layout')
 @section('content')
-
-    <form class="p-3 bg-white mb-3">
+<div class="form-floating mx-1">
+    <form class="p-3 bg-white mb-3 ">
         @csrf
         @method('POST')
         <h2>Formulario Examen final</h2>
         <br>
-        <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">Nombre</label>
-            <input type="text" class="form-control" value="{{old('name')}}" name="name" id="exampleFormControlInput1" placeholder="Nombre">
-            {{--{{$errors->first('name')}}--}}
+        <div class="form-floating mx-1">
+            <input type="text" class="form-control"  name="nombre" id="exampleFormControlInput1" placeholder="Nombre">
+            <label for="nombre" class="form-label">Nombre</label>
         </div>
         <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">Apellido</label>
-            <input type="text" class="form-control" value="{{old('address')}}" name="address" id="exampleFormControlInput1" placeholder="Apellido">
+
+            <input type="text" class="form-control" name="apellido" id="exampleFormControlInput1" placeholder="Apellido">
+            <label for="apellido" class="form-label">Apellido</label>
         </div>
         <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">Carne</label>
-            <input type="text" class="form-control" value="{{old('phone_number')}}" name="phone_number" id="exampleFormControlInput1" placeholder="Carne">
+
+            <input type="text" class="form-control"  name="carne" id="exampleFormControlInput1" placeholder="Carne">
+            <label for="name" class="form-label">Carne</label>
         </div>
         <div class="mb-3">
-            <label for="exampleFormControlInput1" class="form-label">Nota examen Final</label>
-            <input type="text" class="form-control" value="{{old('phone_number')}}" name="phone_number" id="exampleFormControlInput1" placeholder="NotaExamenFinal">
+
+            <input type="text" class="form-control" name="nota" id="exampleFormControlInput1" placeholder="NotaExamenFinal">
+            <label for="nota" class="form-label">Nota examen Final</label>
         </div>
         <div class="mb-3">
             <button type="submit" class="btn btn-success">Guardar <i class="fas fa-save"></i></button>
             <a href="{{url('/')}}" class="btn btn-danger">Cancelar <i class="fas fa-arrow-alt-circle-left"></i></a>
         </div>
+    </div>
 
 @endsection
 
