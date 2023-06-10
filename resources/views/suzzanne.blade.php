1543bde641baa1a1484ea32faa21570ba28724dd
Suzzanne20 <susanacevedo11@hotmail.com>
2023-06-10 08:07:42 -0600

    Creacion ruta en navbar principal a vista nueva

---
+++ resources/views/suzzanne.blade.php
@@ -0,0 +1,18 @@
+@extends('layout')
+
+@section('title', 'Page Title')
+
+@section('sidebar')
+    @parent
+    <br>
+    <h3><div class="text-center" >
+            Registro
+        </div>
+    </h3>
+@endsection
+
+@section('content')
+ 
+<h3>Hola</h3>
+
+@endsection
