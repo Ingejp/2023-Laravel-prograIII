4439d20ae30a1ede29deb8da7d5831c35905be60
Gerardo155 <alimah@miumg.edu.gt>
2023-06-10 08:07:23 -0600

    commit bootstrap

--- resources/views/examenfinal.blade.php
+++ resources/views/examenfinal.blade.php
@@ -1,3 +1,23 @@
+
+.<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+    <title>Examen Final</title>
+
+
+
+
+
+</head>
+<body>
+
+
 <form>
 
     <div class="mb-3">
@@ -26,3 +46,8 @@
     </div>
     <button type="submit" class="btn btn-primary">Submit</button>
 </form>
+
+
+
+</body>
+</html>
