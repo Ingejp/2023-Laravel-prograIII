313cad27bf9d11bb3be60d4174c71f360a21b133
NoeEsquivel <eesquivelc@miumg.edu.gt>
2023-06-10 08:52:46 -0600

    Quinto commit Noe Esquivel

--- resources/views/examen_final_esquivel.blade.php
+++ resources/views/examen_final_esquivel.blade.php
@@ -1,18 +1,26 @@
 @extends('layout')
 @section('content')
-<hr>
 
-<p class="card-text">
-<div class="table table-responsive">
-    <table class="table table-sm table-bordered">
-        <thead>
-        <th>Nombre</th>
-        <th>Apellido</th>
-        <th>Carnet</th>
-        <th>Nota_Examen_Final</th>
-
-        </thead>
-        <tbody>
-        </tbody>
-    </table>
-</div>
+    <div class="form-group">
+        <label for="exampleInputEmail1">Nombre</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    </div>
+    <div class="form-group">
+        <label for="exampleInputPassword1">Apellido</label>
+        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    </div>
+    <div class="form-group">
+        <label for="exampleInputPassword1">carnet</label>
+        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    </div>
+    <div class="form-group">
+        <label for="exampleInputPassword1">nota_examen_final</label>
+        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    </div>
+    <div class="form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheck1">
+        <label class="form-check-label" for="exampleCheck1">Check me out</label>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
