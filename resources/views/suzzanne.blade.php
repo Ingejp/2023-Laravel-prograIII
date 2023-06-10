4c0b27d29c875bb165fa1e49b5d146bc1545c2ef
Suzzanne20 <susanacevedo11@hotmail.com>
2023-06-10 08:23:53 -0600

    creacion de formulario con Bootstrap

--- resources/views/suzzanne.blade.php
+++ resources/views/suzzanne.blade.php
@@ -13,6 +13,30 @@
 
 @section('content')
  
-<h3>Hola</h3>
+<div class="card col-6 ">
+        <h3 class="card-header">Agregar nuevo</h3>
+        <div class="card-body">
+            <p class="card-text">
+            <div class="modal-content">
+                <form action="#" method="POST">
+                    @csrf
+                    <label for="">Nombre</label>
+                    <input type="text" name="nombre" class="form-control">
+                    <label for="">Apellido</label>
+                    <input type="text" name="razon_social" class="form-control">
+                    <label for="">Carnet</label>
+                    <input type="text" name="razon_social" class="form-control">
+                    <label for="">Nota Examen Final</label>
+                    <input type="text" name="razon_social" class="form-control">
+                    <br>
+
+                    <button class="btn btn-dark btn-sm">
+                        <span class="bi bi-plus-circle"></span>  Agregar
+                    </button>
+                </form>
+                </div>
+            </p>
+        </div>
+    </div>
 
 @endsection
